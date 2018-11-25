237f2d9d6b1226aeba015bcf7dde969fc5984980
Julian Cañaveral <jcanaveral@ebfactory.com>
2018-11-25 16:53:40 -0500

    codigo box azul

---
+++ json/subscribers/subscribersTopics.js
@@ -0,0 +1,178 @@
+var subscribersTopics = {
+    topics: [{
+            "json_featuretype": "NewFeatureType",
+            "Department": "IFD",
+            "Subscribers": "6782",
+            "Topic": "Connectivity and Digital Inclusion",
+            "Divisions": "CMF"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "IFD",
+            "Subscribers": "26393",
+            "Topic": "Financial Markets",
+            "Divisions": "CMF"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "CSD",
+            "Subscribers": "58883",
+            "Topic": "Env, Climate Change and Safeguards",
+            "Divisions": "CSD"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "IFD",
+            "Subscribers": "46185",
+            "Topic": "Science, Technology and Innovation",
+            "Divisions": "CTI"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "IFD",
+            "Subscribers": "52854",
+            "Topic": "Fiscal Policy and Management",
+            "Divisions": "FMM"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "SCL",
+            "Subscribers": "33630",
+            "Topic": "Gender and Diversity",
+            "Divisions": "GDI"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "CSD",
+            "Subscribers": "81676",
+            "Topic": "Urban Development and Housing",
+            "Divisions": "HUD"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "IFD",
+            "Subscribers": "33000",
+            "Topic": "Citizen Security and Justice",
+            "Divisions": "ICS"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "IFD",
+            "Subscribers": "48776",
+            "Topic": "Public Management",
+            "Divisions": "ICS"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "INT",
+            "Subscribers": "53167",
+            "Topic": "Trade and Regional Integration",
+            "Divisions": "INT"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "KIC",
+            "Subscribers": "50571",
+            "Topic": "Creative Industries",
+            "Divisions": "KIC"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "KIC",
+            "Subscribers": "56785",
+            "Topic": "Management for Results",
+            "Divisions": "KLD"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "KIC",
+            "Subscribers": "283051",
+            "Topic": "Open Knowledge",
+            "Divisions": "KLD"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "KIC",
+            "Subscribers": "127253",
+            "Topic": "Project Management",
+            "Divisions": "KLD"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "SCL",
+            "Subscribers": "39219",
+            "Topic": "Labor and Pensions",
+            "Divisions": "LMK"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "RES",
+            "Subscribers": "19543",
+            "Topic": "Macroeconomics and Finances",
+            "Divisions": "RES"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "RES",
+            "Subscribers": "3931",
+            "Topic": "Microeconomics and Competitiveness",
+            "Divisions": "RES"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "CSD",
+            "Subscribers": "45659",
+            "Topic": "Agriculture and Food Security",
+            "Divisions": "RND"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "SCL",
+            "Subscribers": "144",
+            "Topic": "Social Issues",
+            "Divisions": "SCL"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "SPD",
+            "Subscribers": "9381",
+            "Topic": "Development Effectiveness",
+            "Divisions": "SPD"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "SCL",
+            "Subscribers": "52762",
+            "Topic": "Early Childhood Development",
+            "Divisions": "SPH"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "SCL",
+            "Subscribers": "54182",
+            "Topic": "Social Protection and Poverty",
+            "Divisions": "SPH"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "INE",
+            "Subscribers": "28468",
+            "Topic": "Transportation",
+            "Divisions": "TSP"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "INE",
+            "Subscribers": "8510",
+            "Topic": "Urban Transport",
+            "Divisions": "TSP"
+        },
+        {
+            "json_featuretype": "NewFeatureType",
+            "Department": "INE",
+            "Subscribers": "50015",
+            "Topic": "Water and Sanitation",
+            "Divisions": "WSA"
+        }
+    ]
+};
